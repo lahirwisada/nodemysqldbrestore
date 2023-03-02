a7483c7fa036520e799ec058dfae5406cd1ef6b2
Lahir Wisada Santoso <lahirwisada@gmail.com>
2023-03-03 00:52:01 +0700

    remove unused code

--- main.js
+++ main.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const { exec,execSync } = require('child_process');
-const cliProgress = require('cli-progress');
 const { Spinner } = require('cli-spinner');
 const { dialog } = require('electron');
 const readline = require('readline');
-const { join } = require('path');
 
 // Define database credentials
 let dbHost = 'localhost';
@@ -79,73 +77,11 @@ rl.question('Enter database port (default 3306): ', (port) => {
             console.log(data);
           });
 
-          // Display progress bar while restoring database
-          // progressBar.start(100, 0);
-          // let progress = 0;
-          // const interval = setInterval(() => {
-          //   progress++;
-          //   progressBar.update(progress);
-          //   if (progress === 100) {
-          //     clearInterval(interval);
-          //     progressBar.stop();
-          //   }
-          // }, 10);
-
           rl.close();
         }else{
           rl.close();
           return;
         }
-        /**
-        dialog.showOpenDialog({
-          filters: [
-            { name: 'SQL files', extensions: ['sql'] }
-          ]
-        }).then(result => {
-          if (result.canceled) {
-            console.log('No file selected');
-            rl.close();
-            return;
-          }
-          const sqlFile = result.filePaths[0];
-
-          // Read SQL file
-          const sql = fs.readFileSync(sqlFile, 'utf8');
-
-          // Define MySQL command to restore database
-          const command = `mysql -h ${dbHost} -P ${dbPort} -u ${dbUser} -p${dbPass} ${dbName} < ${sqlFile}`;
-
-          // Execute MySQL command and display progress bar
-          exec(command, (error, stdout, stderr) => {
-            if (error) {
-              console.error(`Error: ${error.message}`);
-              return;
-            }
-            if (stderr) {
-              console.error(`Error: ${stderr}`);
-              return;
-            }
-            console.log('Database restored successfully!');
-          });
-
-          // Display progress bar while restoring database
-          progressBar.start(100, 0);
-          let progress = 0;
-          const interval = setInterval(() => {
-            progress++;
-            progressBar.update(progress);
-            if (progress === 100) {
-              clearInterval(interval);
-              progressBar.stop();
-            }
-          }, 10);
-
-          rl.close();
-        }).catch(err => {
-          console.error(err);
-          rl.close();
-        });
-        */
       });
     });
   });
